a0a1072b1b64020dbae21165683451cca6760c5c
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2019-01-18 20:45:59 -0500

    Add create products endpoint

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,10 +1,33 @@
 module Api
   module V1
     class ProductsController < ApplicationController
+      skip_before_filter :verify_authenticity_token
+
       def index
         products = Product.all
         render json: {data: products}, status: :ok
       end
+
+      def create
+        product = Product.new(product_params)
+
+        if product.save
+          render json: {
+            status: "Success",
+            data: product
+          }, status: :ok
+        else
+          render json: {
+            status: "Unsaved product",
+            data: product.errors
+          }, status: :unprocessable_entity
+        end
+      end
+
+      private
+        def product_params
+          params.require(:product).permit(:name, :price)
+        end
     end
   end
 end
