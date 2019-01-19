5b1fcfd66ed1570174fe271ffc6396b7dfa0df8b
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2019-01-18 21:08:41 -0500

    Add some validations for a price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
 end
